5a5b3b9cf7823825a3df8b0ee4c5edcf9dd2ce2f
Elton Li <elton.li@ubenefit.net>
2022-04-20 21:39:58 -0500

    Chapter 2 Finished

---
+++ Chapter2-readingList/springBootInActionP1/src/test/java/LearningGuru/springBootInActionP1/SpringBootInActionP1ApplicationTests.java
@@ -0,0 +1,13 @@
+package LearningGuru.springBootInActionP1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootInActionP1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
